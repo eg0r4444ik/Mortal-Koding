6029fd3f05c0c9b323db0519db5a4a5e98f8ccdf
egor <egorchik.rog@yandex.ru>
2023-12-26 22:52:12 +0300

    deployed rabbitmq in docker

--- dispatcher/src/main/java/ru/vsu/rogachev/services/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/vsu/rogachev/services/impl/UpdateProducerImpl.java
@@ -1,6 +1,7 @@
 package ru.vsu.rogachev.services.impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.vsu.rogachev.services.UpdateProducer;
@@ -9,9 +10,16 @@ import ru.vsu.rogachev.services.UpdateProducer;
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 
 }
