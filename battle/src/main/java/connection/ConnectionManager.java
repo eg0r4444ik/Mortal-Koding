30fc3b62b10d534e713716911ae9c52a25356bcb
egor <egorchik.rog@yandex.ru>
2024-01-06 01:39:11 +0300

    add entities and connection manager for java

---
+++ battle/src/main/java/connection/ConnectionManager.java
@@ -0,0 +1,35 @@
+package connection;
+
+import converter.ObjectConverter;
+import converter.impl.ObjectConverterImpl;
+import entities.Problem;
+import entities.Submission;
+import entities.User;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+public class ConnectionManager {
+
+    final RestTemplate restTemplate = new RestTemplate();
+    ObjectConverter objectConverter = new ObjectConverterImpl();
+
+    public User getUser(String handle) throws InterruptedException {
+        Thread.sleep(1000);
+        String response = restTemplate.getForObject("https://codeforces.com/api/user.info?handles=" + handle, String.class);
+        return objectConverter.getUser(response);
+    }
+
+    public List<Submission> getUserSubmissions(String handle) throws InterruptedException {
+        Thread.sleep(1000);
+        String response = restTemplate.getForObject("https://codeforces.com/api/user.status?handle=" + handle, String.class);
+        return objectConverter.getSubmissions(response);
+    }
+
+    public List<Problem> getProblemSet() throws InterruptedException {
+        Thread.sleep(1000);
+        String response = restTemplate.getForObject("https://codeforces.com/api/problemset.problems", String.class);
+        return objectConverter.getProblems(response);
+    }
+
+}
